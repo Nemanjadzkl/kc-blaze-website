eebcfecc83c6545cc337a9ed5abd311d11037ff4
nemanjadzkl <nemanjadzekl@gmail.com>
2024-11-13 16:22:47 +0100

    Fix duplicate volume controls 4

--- js/audio-player.js
+++ js/audio-player.js
@@ -8,26 +8,14 @@ class AudioPlayer {
     initTracks() {
         this.tracks.forEach(track => {
             const playBtn = track.querySelector('.play-btn');
-            playBtn.addEventListener('click', () => this.togglePlay(track));
+            playBtn.addEventListener('click', () => {
+                document.dispatchEvent(new CustomEvent('trackSelected', { 
+                    detail: { 
+                        track: track.dataset.track,
+                        element: track
+                    }
+                }));
+            });
         });
-    }
-
-    togglePlay(track) {
-        if (this.currentTrack === track) {
-            if (document.querySelector('.audio-visualizer-canvas.active')) {
-                document.dispatchEvent(new Event('stopAudio'));
-                track.querySelector('.play-btn i').classList.replace('fa-pause', 'fa-play');
-            } else {
-                document.dispatchEvent(new CustomEvent('playAudio', { detail: { track: track.dataset.track } }));
-                track.querySelector('.play-btn i').classList.replace('fa-play', 'fa-pause');
-            }
-        } else {
-            if (this.currentTrack) {
-                this.currentTrack.querySelector('.play-btn i').classList.replace('fa-pause', 'fa-play');
-            }
-            this.currentTrack = track;
-            document.dispatchEvent(new CustomEvent('playAudio', { detail: { track: track.dataset.track } }));
-            track.querySelector('.play-btn i').classList.replace('fa-play', 'fa-pause');
-        }
     }
 }
